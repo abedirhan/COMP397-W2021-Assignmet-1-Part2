4a459f3b7b79982d8b7f80d35b94930f57294b98
Subarna Bijaya Khadka <khadka.subarnabijaya@gmail.com>
2021-03-01 01:03:15 -0500

    Implemented healthbar update on collision implemented

---
+++ Assets/_Scripts/PlayerHealth.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public float health;
+    public Slider slider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        slider.value = health;
+    }
+
+  
+    private void OnTriggerEnter(Collider other)
+    {
+        
+        if (other.gameObject.tag == "Enemy")
+        {
+            health -= 5;
+        }
+    }
+}
