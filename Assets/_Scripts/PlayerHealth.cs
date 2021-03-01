2dee3531f4618e872bd97ef26586fa3b84a7307e
Subarna Bijaya Khadka <khadka.subarnabijaya@gmail.com>
2021-03-01 02:22:01 -0500

    Implemented teleporting to Level2 on coin collision

--- Assets/_Scripts/PlayerHealth.cs
+++ Assets/_Scripts/PlayerHealth.cs
@@ -1,16 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
     public float health;
     public Slider slider;
+    public int coinCollected;
+    public Text coinText;
     // Start is called before the first frame update
     void Start()
     {
-        
+        coinCollected = 0;
     }
 
     // Update is called once per frame
@@ -25,13 +28,21 @@ public class PlayerHealth : MonoBehaviour
         
         if (other.gameObject.tag == "Enemy")
         {
-            health -= 5;
+            health -= 20;
         }
 
         if (other.gameObject.tag == "coin")
         {
-            health += 10;
+            health += 5;
+            coinCollected += 1;
+            coinText.text = coinCollected.ToString() + " Coins!"; 
             Destroy(other.gameObject);
+            if (coinCollected >= 20)
+            {
+                Debug.Log("Moving to level2" + health);
+                SceneManager.LoadScene("Level2");
+                Cursor.lockState = CursorLockMode.Confined;
+            }
         }
     }
 }
